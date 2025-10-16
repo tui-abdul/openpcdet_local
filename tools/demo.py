c0ca8f9e97b862283c953df1bb642bb1d7b040c2
tui-abdul <abdulhaq.ah@gmail.com>
2025-10-16 23:15:32 +0200

    cfg updat

--- tools/demo.py
+++ tools/demo.py
@@ -95,8 +95,10 @@ def main():
             logger.info(f'Visualized sample index: \t{idx + 1}')
             data_dict = demo_dataset.collate_batch([data_dict])
             load_data_to_gpu(data_dict)
+            
             pred_dicts, _ = model.forward(data_dict)
-            print(pred_dicts[0]['pred_labels'])
+            print(pred_dicts[0]['pred_scores'])
+            
             V.draw_scenes(
                 points=data_dict['points'][:, 1:], ref_boxes=pred_dicts[0]['pred_boxes'],
                 ref_scores=pred_dicts[0]['pred_scores'], ref_labels=pred_dicts[0]['pred_labels']
